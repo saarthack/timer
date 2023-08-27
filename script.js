a79bc07129828d6cf62063b7d0e05a553b7a9559
Sarthak Sharma <ssarthak770@gmail.com>
2023-08-27 12:45:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var h1 = document.querySelector("h1")
+
+var grow = 0
+
+setInterval(function(){
+  if(grow<100){
+    grow += Math.floor(Math.random()*15)
+    h1.innerHTML = grow + "%"
+  }else{
+    grow = 100
+    h1.innerHTML = grow + "%"
+  }
+}, Math.floor(Math.random()*200))
